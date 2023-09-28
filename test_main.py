2af178fd0513fb3359312c1f6fad62524d2bffa0
scoutskylar <scoutskylar@outlook.com>
2023-09-28 00:31:14 -0400

    Add unit test for length-calc

--- test_main.py
+++ test_main.py
@@ -23,3 +23,12 @@ def test_custom_clock_get(client):
 def test_custom_clock_post(client):
     response = client.post('/custom', data={'user': 'Bob Sample'})
     assert 'Welcome, Bob Sample!' in str(response.data)
+
+def test_length_calc_get(client):
+    response = client.get('/length-calc')
+    assert 'Enter a string in the box below' in str(response.data)
+
+def test_length_calc_post(client):
+    response = client.post('/length-calc', data={'string': 'lorem ipsum dolor sit amet'})
+    assert not 'Enter a string in the box below' in str(response.data)
+    assert 'has 26 characters' in str(response.data)
